2ff48ef481e663fb36593a10cf9e07be8ec8c335
Hyegyeong310 <hyegyeong310@gmail.com>
2019-06-30 23:20:27 +0900

    Updates

---
+++ precache-manifest.de230d9866d97b88ee760ccf27aceb1c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0390b747ca823d597d7a1e80f2727fd",
+    "url": "/react-slideshow/index.html"
+  },
+  {
+    "revision": "04ecc5aa9a9d51faabf9",
+    "url": "/react-slideshow/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "c44b846c37ea496b6fcd",
+    "url": "/react-slideshow/static/js/2.34b4fde6.chunk.js"
+  },
+  {
+    "revision": "04ecc5aa9a9d51faabf9",
+    "url": "/react-slideshow/static/js/main.f5c9c7de.chunk.js"
+  },
+  {
+    "revision": "fa3ffe824c723f423242",
+    "url": "/react-slideshow/static/js/runtime~main.4ab1da2e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-slideshow/static/media/logo.5d5d9eef.svg"
+  }
+]);
